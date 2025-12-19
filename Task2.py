d4a715a061445af17c1a1bbf894abd3f6198b117
Nitish Pandey <nitishpandey391@gmail.com>
2025-12-20 00:09:00 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,9 @@
+import math
+num=int(input("Enter a number: "))
+d=math.sqrt(num)
+print(f"The square root of {num} is {d}")
+e=math.log(num)
+print(f"The logarithm of {num} is {e}")
+f=math.radians(num)   #suppose that give data  in degree so converting into radian
+g=math.sin(f)
+print(f"The sine of {num} is {g}")
